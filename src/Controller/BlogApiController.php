1cb516347e53ee9a5b5ef2c1eab142fcdc7f6563
wojiaosasha <sasha.79051@gmail.com>
2020-12-19 04:50:53 +1000

    +

--- src/Controller/BlogApiController.php
+++ src/Controller/BlogApiController.php
@@ -9,6 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogApiController extends AbstractController
 {
+    /**
+     * @Route("/api/posts/{id}", methods={"GET"})
+     */
+    public function write(int $id = 666): Response
+    {
+        // ... edit a post
+        return new Response (
+            '<html><body><h1>Количество заболевших корова-вирусом: '.$id.'</h1></body></html>'
+        );
+    }
+
     // /**
     //  * @Route("/api/posts/{id}", methods={"GET","HEAD"})
     //  */
@@ -16,17 +27,17 @@ class BlogApiController extends AbstractController
     // {
     //     // ... return a JSON response with the post
     // }
-
-    /**
-     * @Route("/api/posts/{id}", methods={"PUT"}) //
-     */
-    public function edit(int $id): Response
-    {
-        // ... edit a post
-        return new Response (
-            '<html><body><h1>Количество заболевших корова-вирусом: '.$id.'</h1></body></html>'
-        );
-    }
+    
+    ///**
+    // * @Route("/api/posts/{id}", methods={"PUT"})
+    // */
+    //public function edit(int $id): Response
+    //{
+    //    // ... edit a post
+    //    return new Response (
+    //        '<html><body><h1>Количество заболевших корова-вирусом: '.$id.'</h1></body></html>'
+    //    );
+    //s}
 }
 
 ?>
